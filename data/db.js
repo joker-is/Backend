9e628ff9f322bef2a3589a771d9215a293299339
Andri Þór <andrithors@hotmail.com>
2019-05-25 14:31:08 +0000

    added env

--- data/db.js
+++ data/db.js
@@ -3,11 +3,9 @@ const mongoose = require('mongoose');
 const MongoClient = require('mongodb').MongoClient;
 const EmailSubscriberSchema = require('../schemas/emailSubscriber');
 
-const connection = mongoose.createConnection(
-  // TODO!: FIX USERNAME AND PASSWORD STRING
-  process.env.DB_CONN,
-  { useNewUrlParser: true }
-);
+const connection = mongoose.createConnection(process.env.DB_CONN, {
+  useNewUrlParser: true,
+});
 
 module.exports = {
   EmailSubscriber: connection.model('EmailSubscribers', EmailSubscriberSchema),
