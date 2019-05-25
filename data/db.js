620a15b01f136c8477bd54ddbd3e5e1e19898b1a
Andri Þór <andrithors@hotmail.com>
2019-05-25 14:28:19 +0000

    added env

--- data/db.js
+++ data/db.js
@@ -1,11 +1,11 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const MongoClient = require('mongodb').MongoClient;
 const EmailSubscriberSchema = require('../schemas/emailSubscriber');
 
 const connection = mongoose.createConnection(
   // TODO!: FIX USERNAME AND PASSWORD STRING
-  'mongodb://<dbuser>:<dbpassword>@ds261096.mlab.com:61096/joker',
-  // 'mongodb://adminUser:XwLFw9f7vZjm@ds261096.mlab.com:61096/joker',
+  process.env.DB_CONN,
   { useNewUrlParser: true }
 );
 
