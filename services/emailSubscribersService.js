9ccfa15b09b7274f6325c55af1db7281d29803c0
Andri Þór <andrithors@hotmail.com>
2019-05-25 19:56:07 +0000

    fixed deactivateEmail

--- services/emailSubscribersService.js
+++ services/emailSubscribersService.js
@@ -26,9 +26,19 @@ const emailSubscribersService = () => {
     });
   };
   const deactivateEmail = (id, callback, onFailure) => {
-    EmailSubscriber.replaceOne({ _id: id }, { active: false }, err => {
+    EmailSubscriber.findById(id, (err, emailSubscriber) => {
       if (err) return onFailure(Error(500));
-      return callback();
+      if (emailSubscriber == null) return onFailure(Error(404));
+      EmailSubscriber.replaceOne(
+        { _id: id },
+        { signUpDate: emailSubscriber.signUpDate },
+        { email: emailSubscriber.email },
+        { active: false },
+        err => {
+          if (err) return onFailure(Error(500));
+          return callback();
+        }
+      );
     });
   };
 
